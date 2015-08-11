185b5530012a46354ddc08eca597a11151b54e8b
Andrea Padgett <agpadgett89@gmail.com>
2015-08-11 11:59:31 -0700

    first tests pass

---
+++ src/main/java/ScrabbleScore.java
@@ -0,0 +1,38 @@
+import java.io.Console;
+import java.util.HashMap;
+
+public class ScrabbleScore{
+    public static void main (String[] args) {}
+
+    public Integer testMethod(Character letter) {
+        HashMap<Character, Integer> letterMap = new HashMap<Character, Integer>();
+        letterMap.put('a', 1);
+        letterMap.put('e', 1);
+        letterMap.put('i', 1);
+        letterMap.put('o', 1);
+        letterMap.put('u', 1);
+        letterMap.put('l', 1);
+        letterMap.put('n', 1);
+        letterMap.put('r', 1);
+        letterMap.put('t', 1);
+        letterMap.put('d', 2);
+        letterMap.put('g', 2);
+        letterMap.put('b', 3);
+        letterMap.put('c', 3);
+        letterMap.put('m', 3);
+        letterMap.put('p', 3);
+        letterMap.put('f', 4);
+        letterMap.put('h', 4);
+        letterMap.put('v', 4);
+        letterMap.put('w', 4);
+        letterMap.put('y', 4);
+        letterMap.put('k', 5);
+        letterMap.put('j', 8);
+        letterMap.put('x', 8);
+        letterMap.put('q', 10);
+        letterMap.put('z', 10);
+        return letterMap.get(letter);
+    }
+
+
+}
---
+++ src/test/java/ScrabbleScoreTest.java
@@ -0,0 +1,21 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.HashMap;
+
+public class ScrabbleScoreTest{
+    @Test
+    public void ScrabbleScore_returnAScrabbleScoreForALetter_1() {
+    ScrabbleScore ScrabbleScoreTest = new ScrabbleScore();
+    Integer score = 1;
+    assertEquals(score, ScrabbleScoreTest.testMethod('a'));
+
+    }
+
+    @Test
+    public void ScrabbleScore_returnScoreForAString_Interger() {
+    ScrabbleScore ScrabbleScoreTest = new ScrabbleScore();
+    Integer score = 4
+    assertEquals(score, ScrabbleScoreTest.testMethod("and"));
+    }
+
+}
